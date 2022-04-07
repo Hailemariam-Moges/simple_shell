5bb75084a70b8cdf8b67a1cb5cf842150ec2e73a
hailemariammoges@gmail.com <hailemariammoges@gmail.com>
2022-04-07 12:54:14 -0700

    Hello, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,17 @@
-#!/bin/bash
+#!/usr/bin/bash
+
 #include <stdio.h>
 #include <string.h>
+/* this is the main function of the program*/
 
-int main()
+int main(void)
 {
-	char str;
-	str=""Programming is like building a multilingual puzzle";
-	puts(str)
-	return(0);
+	char str[100];
+
+	str = ""Programming is like building a multilingual puzzle\n";
+
+	puts(str);
+
+	return (0);
+
 }
