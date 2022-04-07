8c2be2c9be99d00a1a87f45450dc6a6c886a917a
hailemariammoges@gmail.com <hailemariammoges@gmail.com>
2022-04-07 13:09:00 -0700

    Hello, Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 #include <stdio.h>
-#include <string.h>
 
 int main(void)
 {
-	printf("%s", "with proper grammar, but the outcome is a piece of art");
+	printf("with proper grammar, but the outcome is a piece of art");
 
 	return (0);
 }
