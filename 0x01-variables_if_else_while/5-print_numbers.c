bb38a421e461faacf3a7e98141c9b35e1769eb5e
hailemariammoges@gmail.com <hailemariammoges@gmail.com>
2022-04-08 11:03:28 -0700

    Printing base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  main - Entry point
+ *  Return:0
+ */
+
+int main(void)
+
+{
+	int n = 0;
+
+
+	while (n <= 9)
+	{
+		printf("%d", n)
+			n++;
+	}
+	printf('\n');
+	return (0);
+
+
+}
