6dc9abb2fe6155b932048d9b36fc36ca16159f09
hailemariammoges@gmail.com <hailemariammoges@gmail.com>
2022-04-08 09:08:06 -0700

    Print Lowercase Letters

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,5 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
 
 /**
@@ -16,7 +14,7 @@ int main(void)
 	int n;
 
 	int last;
-
+	
 
 
 	srand(time(0));
@@ -25,19 +23,17 @@ int main(void)
 
 	last = n % 10;
 
+
+
 	if (last > 5)
 		printf("Last digit of %i is %i and is greater than 5\n", n, last);
-
 	else if (last == 0)
 		printf("last digit of %i is %i and is 0\n", n, last);
-
-
 	else if (last < 6)
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
-
+	
+	
+	
 	return (0);
 
 }
-
-
-
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *  main - Entry point
+ *  Return:0
+ */
+
+int main(void)
+
+{
+	char c = 'a';
+
+
+	for (int i = 1; i <= 26; i++)
+		(
+		putchar(c);
+		c++;
+
+		 )
+
+
+	putchar('\n');
+	return (0);
+
+
+}
