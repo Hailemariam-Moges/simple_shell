525493cacff40badc93974e53d5cf597a05c8fa2
Hailemariam-Moges <hailemariammoges@gmail.com>
2022-06-16 04:56:17 -0700

    is digit

---
+++ _isdigit.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+/**
+ * _isdigit - test if a character is a number
+ *
+ * @pChar: char
+ *
+ * Return: return 1 if true
+ */
+int _isdigit(char pChar)
+{
+	return (pChar >= '0' && pChar <= '9');
+}
